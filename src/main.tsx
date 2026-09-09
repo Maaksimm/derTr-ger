6e84cab958d6204825a974b7ef3e2b513fc17412
Maksim <pecerskijmaksim13@gmail.com>
2026-09-09 21:05:21 +0300

    fix router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { LanguageProvider } from './i18n/LanguageContext'
 import './styles/tokens.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter basename='/derTr-ger/'>
+		<HashRouter>
 			<LanguageProvider>
 				<App />
 			</LanguageProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>,
 )
